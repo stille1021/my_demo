79a376ec8f75b62b8068c99ce764a8dd0599a2f0
stille <stille1021@yahoo.co.jp>
2016-10-15 13:59:25 +0900

    delete abolished files

--- models/database.js
+++
@@ -1,8 +0,0 @@
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://administrator:123456@localhost:5432/demo';
-
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-  'CREATE TABLE notes(id SERIAL PRIMARY KEY, subject VARCHAR(64), content VARCHAR(255) not null, create_time timestamp, update_time timestamp)');
-query.on('end', () => { client.end(); });
